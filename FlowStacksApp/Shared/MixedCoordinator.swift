a62c31b57b99c3387dfa543cd8056097fbd359fa
johnpatrickmorgan <johnpatrickmorganuk@gmail.com>
2021-12-03 23:05:08 +0000

    Implements withDelaysIfUnsupported

--- FlowStacksApp/Shared/MixedCoordinator.swift
+++ FlowStacksApp/Shared/MixedCoordinator.swift
@@ -3,31 +3,47 @@ import FlowStacks
 
 struct MixedCoordinator: View {
   
-  enum Screen {
-    case number(Int)
+  typealias Screen = Int
+  
+  @State var routes: [Route<Screen>] = [.root(0, embedInNavigationView: true)]
+  
+  var randomRoutes: [Route<Screen>] {
+    let options: [[Route<Screen>]] = [
+      [.root(0, embedInNavigationView: true)],
+      [.root(0, embedInNavigationView: true), .push(1), .push(2), .push(3), .sheet(4, embedInNavigationView: true), .push(5)],
+      [.root(0, embedInNavigationView: true), .push(1), .push(2), .push(3)],
+      [.root(0, embedInNavigationView: true), .push(1), .sheet(2, embedInNavigationView: true), .push(3), .sheet(4, embedInNavigationView: true), .push(5)],
+      [.root(0, embedInNavigationView: true), .sheet(1, embedInNavigationView: true), .cover(2, embedInNavigationView: true), .push(3), .sheet(4, embedInNavigationView: true), .push(5)]
+    ]
+    return options.randomElement()!
   }
   
-  @State var routes: [Route<Screen>] = [.root(.number(1), embedInNavigationView: true)]
   
   var body: some View {
-    Router($routes) { screen, index in
-      switch screen {
-      case .number(let number):
-        NumberView(
-          number: number,
-          presentDoubleCover: { number in
-            routes.presentCover(.number(number * 2), embedInNavigationView: true)
-          },
-          presentDoubleSheet: { number in
-            routes.presentSheet(.number(number * 2), embedInNavigationView: true)
-          },
-          pushNext: { number in
-            routes.push(.number(number + 1))
-          },
-          goBack: { routes.goBack() },
-          popToRoot: { routes.goBackToRoot() }
-        )
-      }
+    Router($routes) { number, _ in
+      NumberView(
+        number: number,
+        presentDoubleCover: { number in
+          routes.presentCover(number * 2, embedInNavigationView: true)
+        },
+        presentDoubleSheet: { number in
+          routes.presentSheet(number * 2, embedInNavigationView: true)
+        },
+        pushNext: { number in
+          routes.push(number + 1)
+        },
+        goBack: { routes.goBack() },
+        goBackToRoot: {
+          $routes.withDelaysIfUnsupported {
+            $0.goBackToRoot()
+          }
+        },
+        goRandom: {
+          $routes.withDelaysIfUnsupported {
+            $0 = randomRoutes
+          }
+        }
+      )
     }
   }
 }
@@ -40,7 +56,8 @@ struct NumberView: View {
   let presentDoubleSheet: (Int) -> Void
   let pushNext: (Int) -> Void
   let goBack: () -> Void
-  let popToRoot: () -> Void
+  let goBackToRoot: () -> Void
+  let goRandom: () -> Void
   
   var body: some View {
     VStack(spacing: 8) {
@@ -49,7 +66,8 @@ struct NumberView: View {
       Button("Present Double (sheet)") { presentDoubleSheet(number) }
       Button("Push next") { pushNext(number) }
       Button("Go back", action: goBack)
-      Button("Pop to root", action: popToRoot)
+      Button("Go back to root", action: goBackToRoot)
+      Button("Go random", action: goRandom)
     }.navigationTitle("\(number)")
   }
 }
--- Sources/FlowStacks/Combined/Binding+withDelaysIfUnsupported.swift
+++ Sources/FlowStacks/Combined/Binding+withDelaysIfUnsupported.swift
@@ -1,6 +1,24 @@
 import Foundation
 import SwiftUI
 
+fileprivate extension Route {
+  
+  enum Style: Hashable {
+    case push, sheet(embedInNavigationView: Bool), cover(embedInNavigationView: Bool)
+  }
+  
+  var style: Style {
+    switch self {
+    case .push:
+      return .push
+    case .sheet(_, let embedInNavigationView):
+      return .sheet(embedInNavigationView: embedInNavigationView)
+    case .cover(_, let embedInNavigationView):
+      return .cover(embedInNavigationView: embedInNavigationView)
+    }
+  }
+}
+
 public extension Binding where Value: Collection, Value.Element: RouteProtocol {
   
   func withDelaysIfUnsupported<Screen>(_ transform: (inout Array<Route<Screen>>) -> Void) where Value == Array<Route<Screen>> {
@@ -11,8 +29,40 @@ public extension Binding where Value: Collection, Value.Element: RouteProtocol {
       return transformed
     }()
     
-    // TODO: make incremental changes to move from initial to transformed in supported steps.
+    let pairs = Array(zip(start, end))
+    let firstDivergingIndex = pairs.dropFirst()
+      .firstIndex(where: { $0.style != $1.style  })
+    ?? pairs.endIndex
     
-    self.wrappedValue = end
+    let initialStep = Array(end[..<firstDivergingIndex] + start[firstDivergingIndex...])
+    var steps = [initialStep]
+    
+    while steps.last!.count > firstDivergingIndex {
+      var newStep = steps.last!
+      var dismissed: Route<Screen>? = newStep.popLast()
+      while dismissed?.style == .push && steps.last!.count > firstDivergingIndex && newStep.last!.style == .push {
+        dismissed = newStep.popLast()
+      }
+      steps.append(newStep)
+    }
+    
+    while steps.last!.count < end.count {
+      var newStep = steps.last!
+      newStep.append(end[steps.last!.count])
+      steps.append(newStep)
+    }
+    
+    self.wrappedValue = steps.first!
+    scheduleRemainingSteps(steps: Array(steps.dropFirst()))
+  }
+  
+  private func scheduleRemainingSteps<Screen>(steps: [[Route<Screen>]]) where Value == Array<Route<Screen>>  {
+    guard let firstStep = steps.first else {
+      return
+    }
+    self.wrappedValue = firstStep
+    DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(650)) {
+      scheduleRemainingSteps(steps: Array(steps.dropFirst()))
+    }
   }
 }
