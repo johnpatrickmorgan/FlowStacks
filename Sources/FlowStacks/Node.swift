f5d4da6482d2f259c2c7f43a654374eabf99d9ba
DavidKmn <davidk.9889@gmail.com>
2022-09-01 12:37:32 +0100

    Fix warning about publishing updates within view updates

--- Sources/FlowStacks/Node.swift
+++ Sources/FlowStacks/Node.swift
@@ -3,39 +3,55 @@ import SwiftUI
 
 /// A view that represents a linked list of routes, each pushing or presenting the next in
 /// the list.
-indirect enum Node<Screen, V: View>: View {
-  case route(Route<Screen>, next: Node<Screen, V>, allRoutes: Binding<[Route<Screen>]>, index: Int, buildView: (Screen) -> V)
-  case end
-  
+struct Node<Screen, V: View>: View {
+
+  indirect enum _Node {
+    case route(Route<Screen>, next: Node<Screen, V>, allRoutes: Binding<[Route<Screen>]>, index: Int, buildView: (Screen) -> V)
+    case end
+  }
+
+  let node: _Node
+  let truncateToIndex: (Int) -> Void
+
+  init(_ node: _Node, truncateToIndex: @escaping (Int) -> Void) {
+    self.node = node
+    self.truncateToIndex = truncateToIndex
+  }
+
   private var isActiveBinding: Binding<Bool> {
-    switch self {
-    case .end, .route(_, next: .end, _, _, _):
+    switch node {
+    case .end:
       return .constant(false)
-    case .route(_, .route, let allRoutes, let index, _):
-      return Binding(
-        get: {
-          allRoutes.wrappedValue.count > index + 1
-        },
-        set: { isShowing in
-          guard !isShowing else { return }
-          guard allRoutes.wrappedValue.count > index + 1 else { return }
-          allRoutes.wrappedValue = Array(allRoutes.wrappedValue.prefix(index + 1))
-        }
-      )
+    case let .route(_, next, allRoutes, index, _):
+      switch next.node {
+      case .end:
+        return .constant(false)
+      case .route:
+        return Binding(
+          get: {
+            allRoutes.wrappedValue.count > index + 1
+          },
+          set: { isShowing in
+            guard !isShowing else { return }
+            guard allRoutes.wrappedValue.count > index + 1 else { return }
+            truncateToIndex(index + 1)
+          }
+        )
+      }
     }
   }
-  
+
   private var pushBinding: Binding<Bool> {
-    switch next {
+    switch next?.node {
     case .route(.push, _, _, _, _):
       return isActiveBinding
     default:
       return .constant(false)
     }
   }
-  
+
   private var sheetBinding: Binding<Bool> {
-    switch next {
+    switch next?.node {
     case .route(.sheet, _, _, _, _):
       return isActiveBinding
     default:
@@ -43,52 +59,54 @@ indirect enum Node<Screen, V: View>: View {
     }
   }
 
-  private var onDismiss: (() -> Void)? {
-    switch next {
-    case .route(.sheet(_, _, let onDismiss), _, _, _, _), .route(.cover(_, _, let onDismiss), _, _, _, _):
-      return onDismiss
-    default:
-      return nil
-    }
-  }
-  
+
   private var coverBinding: Binding<Bool> {
-    switch next {
+    switch next?.node {
     case .route(.cover, _, _, _, _):
       return isActiveBinding
     default:
       return .constant(false)
     }
   }
-  
+
+  private var onDismiss: (() -> Void)? {
+    switch next?.node {
+    case .route(.sheet(_, _, let onDismiss), _, _, _, _),
+        .route(.cover(_, _, let onDismiss), _, _, _, _):
+      return onDismiss
+    default:
+      return nil
+    }
+  }
+
   private var route: Route<Screen>? {
-    switch self {
+    switch node {
     case .end:
       return nil
     case .route(let route, _, _, _, _):
       return route
     }
   }
-  
+
   private var next: Node? {
-    switch self {
+    switch node {
     case .end:
       return nil
     case .route(_, let next, _, _, _):
       return next
     }
   }
-  
+
   @ViewBuilder
   private var screenView: some View {
-    switch self {
+    switch node {
     case .end:
       EmptyView()
     case .route(let route, _, _, _, let buildView):
       buildView(route.screen)
     }
   }
-  
+
   @ViewBuilder
   private var unwrappedBody: some View {
     /// NOTE: On iOS 14.4 and below, a bug prevented multiple sheet/fullScreenCover modifiers being chained
@@ -125,7 +143,7 @@ indirect enum Node<Screen, V: View>: View {
         )
     }
   }
-  
+
   var body: some View {
     if route?.embedInNavigationView ?? false {
       NavigationView {
@@ -141,9 +159,9 @@ indirect enum Node<Screen, V: View>: View {
 /// There are spurious state updates when using the `column` navigation view style, so
 /// the navigation view style is forced to `stack` where possible.
 private var supportedNavigationViewStyle: some NavigationViewStyle {
-  #if os(macOS)
-    .automatic
-  #else
-    .stack
-  #endif
+#if os(macOS)
+  .automatic
+#else
+  .stack
+#endif
 }
--- Sources/FlowStacks/Router.swift
+++ Sources/FlowStacks/Router.swift
@@ -1,10 +1,26 @@
 import Foundation
 import SwiftUI
+import Combine
+
+private final class RouterPathDismissalHandler: ObservableObject {
+
+  var lastDismissedIndex: AnyPublisher<Int, Never> {
+    _lastDismissedIndex.eraseToAnyPublisher()
+  }
+
+  private let _lastDismissedIndex = PassthroughSubject<Int, Never>()
+
+  func dismissedIndex(_ index: Int) {
+    _lastDismissedIndex.send(index)
+  }
+}
 
 /// Router converts an array of pushed / presented routes into a view.
 public struct Router<Screen, ScreenView: View>: View {
   /// The array of routes that represents the navigation stack.
   @Binding var routes: [Route<Screen>]
+
+  @StateObject private var pathDismissHandler = RouterPathDismissalHandler()
 
   /// A closure that builds a `ScreenView` from a `Screen`and its index.
   @ViewBuilder var buildView: (Screen, Int) -> ScreenView
@@ -22,15 +38,23 @@ public struct Router<Screen, ScreenView: View>: View {
     routes
       .enumerated()
       .reversed()
-      .reduce(Node<Screen, ScreenView>.end) { nextNode, new in
+      .reduce(Node<Screen, ScreenView>(.end, truncateToIndex: { _ in })) { nextNode, new in
         let (index, route) = new
-        return Node<Screen, ScreenView>.route(
-          route,
-          next: nextNode,
-          allRoutes: $routes,
-          index: index,
-          buildView: { buildView($0, index) }
+        return Node<Screen, ScreenView>(
+          .route(
+            route,
+            next: nextNode,
+            allRoutes: $routes,
+            index: index,
+            buildView: { buildView($0, index) }
+          ),
+          truncateToIndex: { index in
+            pathDismissHandler.dismissedIndex(index)
+          }
         )
+      }
+      .onReceive(pathDismissHandler.lastDismissedIndex) { index in
+        routes = Array(routes.prefix(index))
       }
   }
 }
